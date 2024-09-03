1b60dd5a382763b04fc9f4f35a0df6d4abc7e6b9
Rick Ross <rick.ross@temporal.io>
2024-09-03 17:32:06 -0400

    Changed shell files to use setcloudenv.sh

--- java/sendcloudapproval.sh
+++ java/sendcloudapproval.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# This is using my temporal email
-export TEMPORAL_ADDRESS=rick-ross.a2dd6.tmprl.cloud:7233
-export TEMPORAL_NAMESPACE=rick-ross.a2dd6
-# this is with my personal email account
-# export TEMPORAL_ADDRESS=rick-ross-dev.sdvdw.tmprl.cloud:7233
-# export TEMPORAL_NAMESPACE=rick-ross-dev.sdvdw
-export TEMPORAL_CERT_PATH="/Users/rickross/dev/samples-server/tls/client-only/mac/rick-ross.pem"
-export TEMPORAL_KEY_PATH="/Users/rickross/dev/samples-server/tls/client-only/mac/rick-ross.key"
+source ../setcloudenv.sh
 ENCRYPT_PAYLOADS=$2 ./gradlew -q execute -PmainClass=io.temporal.samples.moneytransfer.TransferApprover -Parg=$1
-# where TRANSFER-EZF-249 is the workflowId
-#./gradlew -q execute -PmainClass=io.temporal.samples.moneytransfer.TransferApprover -Parg=$1
--- ui/startcloudwebui.sh
+++ ui/startcloudwebui.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-# This is using my temporal email
-export TEMPORAL_ADDRESS=rick-ross.a2dd6.tmprl.cloud:7233
-export TEMPORAL_NAMESPACE=rick-ross.a2dd6
-# this is with my personal email account
-#export TEMPORAL_ADDRESS=rick-ross-dev.sdvdw.tmprl.cloud:7233
-#export TEMPORAL_NAMESPACE=rick-ross-dev.sdvdw
-export TEMPORAL_CERT_PATH="/Users/rickross/dev/samples-server/tls/client-only/mac/rick-ross.pem"
-export TEMPORAL_KEY_PATH="/Users/rickross/dev/samples-server/tls/client-only/mac/rick-ross.key"
+source ../setcloudenv.sh
 ENCRYPT_PAYLOADS=$1 ./gradlew run --console=plain
 
 echo "Navigate to http://localhost:7070/"
